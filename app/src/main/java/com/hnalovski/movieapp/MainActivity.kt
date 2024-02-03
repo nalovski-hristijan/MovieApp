3981d40f45d4eed6d44454220fa8ccfd38920846
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-02-03 04:56:31 +0100

    Initial commit

---
+++ app/src/main/java/com/hnalovski/movieapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.hnalovski.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hnalovski.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        content()
+    }
+}
+
+@Composable
+fun MainContent() {
+    Text(text = "Hello")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApp {
+        MainContent()
+    }
+}
