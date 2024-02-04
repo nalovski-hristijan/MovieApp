068d1d1835602d6a47e40d3c5108d956a54665f2
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-02-04 20:01:34 +0100

    Added rows for the movies that are going to be listed

--- app/src/main/java/com/hnalovski/movieapp/MainActivity.kt
+++ app/src/main/java/com/hnalovski/movieapp/MainActivity.kt
@@ -1,15 +1,43 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.hnalovski.movieapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hnalovski.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +45,89 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                MainContent()
+                MainContent(it)
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyApp(content: @Composable () -> Unit) {
+fun MyApp(content: @Composable (PaddingValues) -> Unit) {
     MovieAppTheme {
-        content()
+        Scaffold(topBar = {
+            TopAppBar(
+                title = { Text(text = "Movies") },
+                modifier = Modifier.shadow(5.dp),
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Magenta)
+            )
+        }) {
+            content(it)
+
+        }
     }
 }
 
 @Composable
-fun MainContent() {
-    Text(text = "Hello")
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+
+            }
+            Text(text = movie)
+        }
+
+
+    }
+}
+
+@Composable
+fun MainContent(
+    paddingValues: PaddingValues,
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "47 Ronin",
+        "Pacific Rim",
+        "Super Mario",
+        "Fantastic Four",
+        "John Wick"
+    )
+) {
+    Surface(
+        modifier = Modifier.padding(top = paddingValues.calculateTopPadding()),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            LazyColumn {
+                items(items = movieList) {
+                    MovieRow(movie = it)
+
+                }
+            }
+        }
+
+    }
 }
 
 
@@ -40,6 +135,6 @@ fun MainContent() {
 @Composable
 fun GreetingPreview() {
     MyApp {
-        MainContent()
+        MainContent(it)
     }
 }
