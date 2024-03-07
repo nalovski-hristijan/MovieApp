ad28e673458e20e6b080a52f0e2ccd82b95f9bf9
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-03-07 14:15:13 +0100

    Added expand button on movie row to see more info about movie

--- app/src/main/java/com/hnalovski/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/hnalovski/movieapp/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.hnalovski.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,38 +13,48 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.hnalovski.movieapp.R
 import com.hnalovski.movieapp.model.Movie
 import com.hnalovski.movieapp.model.getMovies
 
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -70,14 +81,52 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                     contentScale = ContentScale.Crop,
                 )
 
-              //  Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
-
             }
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(text = movie.title, style = MaterialTheme.typography.headlineSmall)
-                Text(text = "Director ${movie.director}", style = MaterialTheme.typography.bodySmall)
+                Text(
+                    text = "Director ${movie.director}",
+                    style = MaterialTheme.typography.bodySmall
+                )
                 Text(text = "Released ${movie.year}", style = MaterialTheme.typography.bodySmall)
 
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        }, modifier = Modifier.padding(6.dp))
+
+                        HorizontalDivider(modifier = Modifier.padding(3.dp))
+                        
+                        Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.bodySmall )
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.bodySmall )
+                        Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.bodySmall )
+
+                    }
+                }
+
+
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
+                )
             }
 
         }
